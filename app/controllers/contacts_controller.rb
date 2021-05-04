3aedee15f04e0a0a7ae2422475e80a92501893d3
VitamiN <scoundrel98@gmail.com>
2021-05-04 19:32:41 +0300

    add contact model valitation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,12 @@ class ContactsController < ApplicationController
 
     def create 
         @contact = Contact.new(contacts_params)
-        @contact.save
+        
+        if @contact.valid?
+            @contact.save
+        else
+            render action: 'new'
+        end
     end
 
     private
