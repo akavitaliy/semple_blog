018993df5095be09b2459ff51c46d31fde490275
VitamiN <scoundrel98@gmail.com>
2021-05-05 19:42:58 +0300

    implement articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,18 @@
 class ArticlesController < ApplicationController
   
+  def index 
+    @articles = Article.all
+  end
+  
   def show 
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
   
   def new             
   end  
   
   def create
-    @articles = Article.new(articles_params)
+    @article = Article.new(articles_params)
 
     if @articles.valid?
       @articles.save
