c14a71c3263857613e3586983d60d72061bcfbd3
VitamiN <scoundrel98@gmail.com>
2021-05-15 22:48:09 +0300

    fixed form edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,20 +12,34 @@ class ArticlesController < ApplicationController
   end  
   
   def create
-    @article = Article.new(articles_params)
+    @article = Article.new(article_params)
 
-    if @article.valid?
-        @article.save
+    if @article.save
         redirect_to @article
     else
       render action: 'new'
     end
   end
 
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
+  def update 
+    @article = Article.find(params[:id])
+
+    
+    if @article.update(article_params)
+        redirect_to @article
+    else
+      render action: 'edit'
+    end
+  end
+
   private
 
-  def articles_params
-    params.require(:article).permit(:title, :text)
+  def article_params
+    params.require(:article).permit(:title, :text,)
   end
   
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get '/' => 'home#index'
 
-  #get 'contacts' => 'contacts#new'
 
-  resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/index'
+
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
+  resources :articles do
+    resources :comments
+  end
 end
