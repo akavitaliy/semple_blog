fb9200ae085d9d15a0488f89914a7e11529231bc
VitamiN <scoundrel98@gmail.com>
2021-06-12 21:56:21 +0300

    add better sign in and sign out links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create]
   
   def index 
     @articles = Article.order "created_at DESC"
