26ba372e6b88afd0686f3c846664459f63a73b91
VitamiN <scoundrel98@gmail.com>
2021-06-16 23:06:28 +0300

    add menu in head

--- db/migrate/20210613115040_add_username.rb
+++ db/migrate/20210613115040_add_username.rb
@@ -1,5 +1,6 @@
 class AddUsername < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :username, :string
+    add_index :users, :username, unique: true
   end
 end
--- db/migrate/20210613121251_add_username_index.rb
+++
@@ -1,5 +0,0 @@
-class AddUsernameIndex < ActiveRecord::Migration[6.1]
-  def change
-    add_index :users, :username, :unique =>  true 
-  end
-end
